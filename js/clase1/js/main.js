99486027c3ccba1feb1d0f8bea5d75aa46d793e5
NayeliAndrade <lucianayeli@outlook.com>
2022-03-09 14:57:33 -0600

    clase 1 PB

---
+++ js/clase1/js/main.js
@@ -0,0 +1,36 @@
+/* alert('hola');  *//* funcion */
+
+/* declarar y asignar */
+var nombre = 'Nayeli';
+console.log(nombre);
+
+/* tipos de datos */
+var num1 = 21; /* int */
+var num2 = 254.54; /* float */
+console.log(num1);
+
+var casado = true; /* booleano t o f */
+casado = false; /* reasignar */
+if (casado == true) {
+    console.log('Felicidades');
+}
+console.log(casado);
+
+/* tipado de datos
+saber que tipo de dato es 
+int num3 = 215
+float num4 = 22.45 */
+
+var nombre = 'Nayeli';
+var usuario = nombre;
+
+var edad = 22;
+if (edad >18) {
+    console.log('Eres mayor');
+}
+
+var nombre = prompt('cual es tu nombre');
+console.log(nombre);
+
+/* console.log(prompt('cual es tu nombre')); */
+
