df7551a44634aee47932b66079bb329cd06d33f8
NayeliAndrade <lucianayeli@outlook.com>
2022-04-09 14:54:46 -0500

    clase 5

---
+++ js/clase5/js/main.js
@@ -0,0 +1,51 @@
+/* algoritmos de ordenamiento y algoritmos de aleatoridad devgabo */
+
+/* var lista = [1,8,4,7,3,6,5,2,0]; */
+/* typeof(123)*/
+
+/* for (let index = 0; index < lista.length; index++) {
+    console.log(lista[index]);    
+} */
+
+
+/* var index = 0; 
+while (index < lista.length){
+    console.log(lista[index]);
+    index++; */
+    /* index = index + 5; */
+
+
+/* var index = 0;
+do {
+    console.log(index);
+    index = index + 5;
+} while (index <= 50); */
+
+/* tipos de datos */
+/* undefined y null */
+
+/* var usuario = null; */
+
+/* o==false */
+/* ''==false */
+/* ''==0 */
+
+document.write('perro');
+/* el punto significa que primero tienes que acceder a document */
+/* document es un objeto que guarda info */
+/* dom todo lo que puedes manipular */
+
+/* seleccionar elementos */
+document.getElementById('perro');
+
+document.getElementsByTagName('div')[0];
+/* grupo de html, HTML COLECTION */
+
+document.getElementsByClassName('bgblue')[1];
+
+var div1 = document.getElementsByTagName('div')[0];
+var div2 = document.getElementsByClassName('bgblue')[1];
+
+/* div1.style.backgroundColor *//* propiedades */
+
+div1.style.backgroundColor = 'blue';
