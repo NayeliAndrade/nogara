b1ab3f70dc65bc08e2d5bf9747237c298a266e2d
NayeliAndrade <lucianayeli@outlook.com>
2022-06-15 14:45:29 -0500

    estatus de las tareas

--- js/todolist/js/main.js
+++ js/todolist/js/main.js
@@ -20,7 +20,8 @@ function Datos(){
         nombreTarea: texto.value
         }
         listaCompleta.push(crearTarea);
-    console.log(JSON.stringify(listaCompleta));
+    /* console.log(JSON.stringify(listaCompleta)); */
+    setDatos();
     imprimirEnPantalla();
     limpiarInput(texto.value);
 }
@@ -33,16 +34,17 @@ const imprimirEnPantalla = ()=>{
     <button class="color" id="estatus_${index}" onclick="estatus(${index})"></button>
     ${listaCompleta[index++].nombreTarea}
     <div class="iconos">
-        <span class="editar"><i class="fa-solid fa-pencil"></i></span>
+        <span class="editar" onclick="editar()"><i class="fa-solid fa-pencil"></i></span>
         <span class="iconoBorrar" onclick="borrar()"><i class="fa-solid fa-trash-can"></i></i></span>
         </div>
     `;
     guardarLista.appendChild(li);
 }
 
-const estatus = () => {
+const estatus = (numero) => {
     let colores = ["red", "yellow", "green"];
-    let btnColor = document.getElementsByClassName('color')[0];
+    let btnColor = document.getElementById('estatus_'+ numero);
+     
     btnColor.style.backgroundColor = colores[numeroColor];
     numeroColor++;
     if (numeroColor === colores.length){
@@ -53,7 +55,7 @@ const estatus = () => {
     }
 }
 
-/* const setDatos = (crearTarea) => {
+const setDatos = () => {
     let datos = JSON.stringify(listaCompleta);
     console.log(datos);
     localStorage.setItem('lista',datos);
@@ -63,8 +65,16 @@ const estatus = () => {
 const getDatos = () => {
     let bd = localStorage.getItem('lista');
     listaCompleta = JSON.parse(bd);
-    console.log(bd);
-    console.log(bd.length);
-    Datos(JSON.parse(bd));
-} */
+    /* console.log(bd);
+    console.log(bd.length); */
+}
 /* alt + 96 backtics*/
+
+
+/* const borrar = () => {   
+    console.log('sd');
+}
+
+const editar = () => {
+    console.log('sdf');
+} */
